ab7fb2147e70206da4b4571b8fc002540534c737
Skjalgn <skjalgn@stud.ntnu.no>
2023-09-14 11:45:31 +1000

    Done with the site layout. Everythings working atm

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -10,4 +10,12 @@ document.getElementById("cityput").addEventListener('keypress', function (e) {
 });
 document.getElementById("citybtn").addEventListener('click', newcity);
 
+function override(mood) {
+  var newcity = document.getElementById("cityput").value;
+  window.location.href = `/?city=${newcity}&override=${mood}`;
+}
 
+
+function gotovideo(link){
+  window.open(link, '_blank').focus();
+}
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 router.get("/", async function (req, res) {
   const YoutubeApiKey = 'AIzaSyC9xhWzcvS4pqF14DFMntlDABUDjyGcsjo';
   const openWeatherApiKey = 'a1b18af47376495368ad011abb3ac86e';
-  var music = req.query.music || 'Summer music';
-  var weatherpic = req.query.weatherpic || 'images/Sunny.svg';
+  const override = req.query.override || "";
   const city = req.query.city || 'Brisbane';
 
   const openWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}`;
@@ -14,47 +13,81 @@ router.get("/", async function (req, res) {
   const currentDate = new Date();
   const currentMonth = currentDate.toLocaleString('en-US', { month: 'long' });
   const weatherDataPromise = fetch(openWeatherApiUrl)
+
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status 1: ${response.status}`);
       }
       return response.json();
     })
     .then((weatherData) => {
+      console.log("--------------------")
       const temperature = (weatherData.main.temp - 273.15).toFixed(2);
       const description = weatherData.weather[0].description;
-      console.log(description);
-      if (description.includes('rain')) {
-        console.log('rain');
-      }
-      if (currentMonth == 'December') {
-        music = 'christmas music';
+
+      const keywordData = {
+        'clear': {
+          music: 'Summer music',
+          picture: 'images/Sunny.svg',
+        },
+        'christmas': {
+          music: 'Christmas music',
+          picture: 'images/Christmas.svg',
+        },
+        'spooky': {
+          music: 'Spooky music',
+          picture: 'images/Spooky.svg',
+        },
+        'love': {
+          music: 'Love music',
+          picture: 'images/Love.svg',
+        },
+        'rain': {
+          music: 'Rainy day music',
+          picture: 'images/Rainy.svg',
+        },
+        'cloud': {
+          music: 'Cloudy music',
+          picture: 'images/Cloudy.svg',
+        },
+        'snow': {
+          music: 'Snowy music',
+          picture: 'images/Snowy.svg',
+        },
+        'thunder': {
+          music: 'Thunder ACDC',
+          picture: 'images/Thunder.svg',
+        },
+      };
+
+      var music = '';
+      var weatherpic = '';
+      console.log(keywordData);
+
+      if (override) {
+        music = keywordData[override].music;
+        weatherpic = keywordData[override].picture;
+      } else if (currentMonth == 'December') {
+        music = 'Christmas music';
         weatherpic = 'images/Christmas.svg';
       } else if (currentMonth == 'October') {
-        music = 'spooky music';
+        music = 'Spooky music';
         weatherpic = 'images/Spooky.svg';
       } else if (currentMonth == 'February') {
-        music = 'love music';
+        music = 'Love music';
         weatherpic = 'images/Love.svg';
       } else if (description.includes('rain')) {
-        weatherpic = 'images/Rainy.svg';
-        music = 'rainy day music';
+        music = keywordData['rain'].music;
+        weatherpic = keywordData['rain'].picture;
       } else if (description.includes('cloud')) {
-        weatherpic = 'images/Cloudy.svg';
-        music = 'Cloudy music';
-      } else if (description.includes('clear')) {
-        weatherpic = 'images/Sunny.svg';
+        music = keywordData['cloud'].music;
+        weatherpic = keywordData['cloud'].picture;
+      } else {
         music = 'Summer music';
-      } else if (description.includes('snow')) {
-        weatherpic = 'images/Snowy.svg';
-        music = 'Snowy music';
-      } else if (description.includes('thunder')) {
-        weatherpic = 'images/Thunder.svg';
-        music = 'Thunder ACDC';
+        weatherpic = 'images/Sunny.svg';
       }
-      const musicreq = music
-      const weatherpicreq = weatherpic
-      
+
+
       // YOUTUBE API
 
       const YoutubeApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${YoutubeApiKey}&q=${music}&part=snippet&type=video&videoEmbeddable=true`;
@@ -72,7 +105,8 @@ router.get("/", async function (req, res) {
             const videoId = video.id.videoId;
             const videoTitle = video.snippet.title;
             const videoThumbnail = video.snippet.thumbnails.default.url;
-            return { title: videoTitle, id: videoId, thumbnail: videoThumbnail };
+            const youtubeLink = `https://www.youtube.com/watch?v=${videoId}`;
+            return { title: videoTitle, id: videoId, thumbnail: videoThumbnail, link: youtubeLink};
           });
           return Promise.all(videoPromises);
         })
@@ -81,14 +115,14 @@ router.get("/", async function (req, res) {
         });
 
 
-      Promise.all([weatherDataPromise, videodataPromises, currentMonth, city, weatherpic, music]) // Commented out videodataPromises
+      Promise.all([weatherDataPromise, videodataPromises, currentMonth, city, weatherpic, music])
         .then(([weatherData, videodata, month, city, weatherpic, music]) => {
-          res.render('index', { title: 'Express', videos: videodata, weather: weatherData, month: month, city: city, weatherpic: weatherpic, music: music });
+          res.render('index', { title: 'WeatherBeats', videos: videodata, weather: weatherData, month: month, city: city, weatherpic: weatherpic, music: music });
         })
         .catch((error) => {
           console.error('Error:', error);
         });
-      return { temperature, description, musicreq, weatherpicreq };
+      return { temperature, description, music, weatherpic };
     })
     .catch((error) => {
       console.error('Error:', error);
